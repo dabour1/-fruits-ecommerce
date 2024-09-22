7f65cdd849bb962e158e3c140e42cce46b1da412
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-23 00:34:14 +0300

    add RoleRepository

---
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/repositorys/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.springBoot.fruits_ecommerce.repositorys;
+
+import com.springBoot.fruits_ecommerce.enums.RoleName;
+import com.springBoot.fruits_ecommerce.models.Role;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+   
+        Optional<Role>  findByName(RoleName name); 
+     
+
+}
