cc61909df4f4484c745154b568f599a241a11972
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-22 03:36:48 +0300

    Initializing project

---
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/FruitsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.fruits_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsEcommerceApplication.class, args);
+	}
+
+}
