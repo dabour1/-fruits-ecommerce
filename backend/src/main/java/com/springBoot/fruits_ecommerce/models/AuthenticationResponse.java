db6da6cbb59c5cff5f62aeb4472fb6948a223666
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-23 01:48:04 +0300

    add AuthenticationResponse model

---
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/models/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.springBoot.fruits_ecommerce.models;
+
+public class AuthenticationResponse {
+    private String token;
+    public AuthenticationResponse(String token) {
+        this.token = token;
+    }
+    public String getToken() {
+        return token;
+    }
+
+}
