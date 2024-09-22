e4a8246bfd0c2e37438c6b11ff14eab831436067
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-23 01:29:01 +0300

    add ApplicationConfig and  UserDetailsService

---
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/config/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package com.springBoot.fruits_ecommerce.config;
+
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+ 
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+ 
+
+import com.springBoot.fruits_ecommerce.repositorys.UserRepository;
+
+@Configuration
+ 
+public class ApplicationConfig {
+
+  @Autowired
+  private UserRepository repository;
+
+  
+
+   
+}
