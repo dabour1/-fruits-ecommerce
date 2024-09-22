c170aa6c491c22d94fe3c736e92335e01c90eeb8
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-22 23:59:40 +0300

    add User entity

---
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/models/User.java
@@ -0,0 +1,49 @@
+package com.springBoot.fruits_ecommerce.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "username is required")
+    @NotNull(message ="username cannot be null")
+    private String username;
+    @NotBlank(message = "email is required")
+    @NotNull(message ="email cannot be null")
+    @Email(message = "Email should be valid")
+    private String email;
+    @NotBlank(message = "password is required")
+    @NotNull(message ="password cannot be null")
+    @Size(min = 8, max = 50, message = "password must be between 2 and 50 characters")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+}
