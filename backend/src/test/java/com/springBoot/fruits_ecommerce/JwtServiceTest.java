02530978218bf1c87e908f240f310cfa2e7c3214
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-23 18:33:02 +0300

    Added test for generateToken method in JwtServiceTest, including setup of User object and RoleName enum.

--- backend/src/test/java/com/springBoot/fruits_ecommerce/JwtServiceTest.java
+++ backend/src/test/java/com/springBoot/fruits_ecommerce/JwtServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.springBoot.fruits_ecommerce.enums.RoleName;
+import com.springBoot.fruits_ecommerce.models.User;
 import com.springBoot.fruits_ecommerce.services.JwtService;
 
 import javax.crypto.SecretKey;
@@ -21,6 +23,7 @@ import static org.mockito.Mockito.*;
 public class JwtServiceTest {
     private JwtService jwtService;
     private SecretKey secretKey;
+    private  User user;
 
     @BeforeEach
     public void setUp() {
@@ -28,6 +31,19 @@ public class JwtServiceTest {
         byte[] keyBytes = Decoders.BASE64.decode("12df32e69496a167c9d6196abec4bd4a25858e8da5fa4fe150ac9e03431ca36b");
         secretKey = Keys.hmacShaKeyFor(keyBytes);
         doReturn(secretKey).when(jwtService).getSigningKey();
+          user = new User();
+        user.setEmail("user@example.com");
+    }
+    @Test
+    public void testGenerateToken() {
+        
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("roles", RoleName.CLIENT);
+ 
+        String token = jwtService.generateToken(user, extraClaims);
+ 
+        assertNotNull(token);
+        assertTrue(token.length() > 0);
     }
 
 }
