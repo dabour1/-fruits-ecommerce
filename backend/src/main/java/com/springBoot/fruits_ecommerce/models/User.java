0f7fd8807ad8134ecc212f6c00131d6060cce610
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-09-26 03:52:18 +0300

    Remove unnecessary import statement for @AllArgsConstructor in User.java model.

--- backend/src/main/java/com/springBoot/fruits_ecommerce/models/User.java
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/models/User.java
@@ -17,8 +17,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
