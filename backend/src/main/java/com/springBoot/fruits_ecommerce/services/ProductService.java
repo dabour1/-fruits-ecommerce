7322383b1bbecb386f6e7df00733855f55544d94
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-10-07 01:01:57 +0300

    "Added autowired ProductRepository, getProductById method, and empty save method to ProductService class."

--- backend/src/main/java/com/springBoot/fruits_ecommerce/services/ProductService.java
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/services/ProductService.java
@@ -1,5 +1,21 @@
 package com.springBoot.fruits_ecommerce.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springBoot.fruits_ecommerce.exception.ResourceNotFoundException;
+import com.springBoot.fruits_ecommerce.models.Product;
+import com.springBoot.fruits_ecommerce.repositorys.ProductRepository;
+
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    public void save(Product product) {
+
+    }
 
 }
