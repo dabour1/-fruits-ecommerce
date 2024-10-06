62148e7e83df711b907da15a8d7102c4fe7ce82a
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-10-07 00:48:54 +0300

    add ImageService and FileStorageException

---
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/services/ImageService.java
@@ -0,0 +1,61 @@
+package com.springBoot.fruits_ecommerce.services;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.core.io.Resource;
+
+import org.springframework.core.io.UrlResource;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+
+import java.nio.file.StandardCopyOption;
+
+import java.util.UUID;
+
+import java.nio.file.Paths;
+
+@Service
+public class ImageService {
+
+    private final String uploadDir = "files/";
+
+    public String saveImage(MultipartFile image) {
+
+        if (image.isEmpty()) {
+            throw new IllegalArgumentException("Image file is empty");
+        }
+        try {
+            String imageName = UUID.randomUUID().toString() + "_" +
+                    image.getOriginalFilename();
+            Path imagePath = Paths.get(uploadDir, imageName);
+            Files.createDirectories(imagePath.getParent());
+            Files.copy(image.getInputStream(), imagePath,
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            return imageName;
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving file: " +
+                    image.getOriginalFilename(), e);
+        }
+    }
+
+    public Resource loadImage(String imageName) {
+        Path imagePath = Paths.get(uploadDir).resolve(imageName).normalize();
+        try {
+            Resource resource = new UrlResource(imagePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read file: " + imageName);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading file: " + imageName, e);
+        }
+
+    }
+}
