84d51c34aeb5a537790db91e304e909ebf43f512
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-10-07 03:37:50 +0300

    "Added GET endpoints to ProductController for retrieving all products and a single product by ID, and updated ProductService to support these new endpoints."

--- backend/src/main/java/com/springBoot/fruits_ecommerce/controllers/ProductController.java
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.springBoot.fruits_ecommerce.models.AddProductRequest;
 import com.springBoot.fruits_ecommerce.models.Product;
 import com.springBoot.fruits_ecommerce.services.ProductService;
-
+import org.springframework.data.domain.Page;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
@@ -34,6 +35,21 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@Valid @RequestBody AddProductRequest addProductRequest) {
         Product product = productService.createProduct(addProductRequest);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getAllProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<Product> products = productService.getAllProducts(page, size);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+
     }
 
     @DeleteMapping("/{id}")
--- backend/src/main/java/com/springBoot/fruits_ecommerce/services/ProductService.java
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/services/ProductService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+
 import org.springframework.data.domain.Pageable;
 import com.springBoot.fruits_ecommerce.exception.ResourceNotFoundException;
 import com.springBoot.fruits_ecommerce.models.AddProductRequest;
