9470593b64500487e2359c5002a85d665a43f3fc
Ahmed Dabour <dabour@bonyaninnovation.com>
2024-10-19 20:51:08 +0300

    Removed unnecessary imports in AuthApiController.java

--- backend/src/main/java/com/springBoot/fruits_ecommerce/api/AuthApiController.java
+++ backend/src/main/java/com/springBoot/fruits_ecommerce/api/AuthApiController.java
@@ -3,7 +3,6 @@ package com.springBoot.fruits_ecommerce.api;
 import com.springBoot.fruits_ecommerce.models.AuthenticationRequest;
 import com.springBoot.fruits_ecommerce.models.AuthenticationResponse;
 import com.springBoot.fruits_ecommerce.models.RegistrationRequest;
-import com.springBoot.fruits_ecommerce.mappers.MapRegistrationRequest;
 import com.springBoot.fruits_ecommerce.services.AuthenticationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.mariadb.jdbc.internal.logging.Logger;
-import org.mariadb.jdbc.internal.logging.LoggerFactory;
 
 import javax.validation.Valid;
 
